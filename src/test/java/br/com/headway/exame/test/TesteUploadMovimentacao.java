811a3e0d2440a9c188c5c0ef15d961907531e640
danilovsantos <danilo_valente@hotmail.com>
2016-12-15 10:42:34 -0200

    ajustes

--- src/test/java/br/com/headway/exame/test/TesteUploadMovimentacao.java
+++ src/test/java/br/com/headway/exame/test/TesteUploadMovimentacao.java
@@ -1,17 +1,18 @@
 package br.com.headway.exame.test;
 
 import br.com.headway.exame.dao.MovimentacaoDao;
+import br.com.headway.exame.dao.MovimentacaoDaoImpl;
 import br.com.headway.exame.model.Movimentacao;
 
 public class TesteUploadMovimentacao {
 
 	public static void main(String[] args) {
 			
-		MovimentacaoDao dao = new MovimentacaoDao();
+		MovimentacaoDao dao = new MovimentacaoDaoImpl();
 		Movimentacao mov = dao.getArquivoMovimentacao();
 		
-		System.out.println(dao.filialMaiorFaturamentoPeriodo(mov));
-		System.out.println(dao.filialMenorFaturamentoPeriodo(mov));
+		System.out.println(dao.filialMaiorFaturamentoPeriodo(mov).getNome());
+		System.out.println(dao.filialMenorFaturamentoPeriodo(mov).getNome());
 		System.out.println(dao.mesMaiorFaturamento(mov));
 
 	}
